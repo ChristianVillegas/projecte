009d860bea157f708b5de71886ea93deacb75ac3
Christian Villegas <cf18christian.villegas@iesjoandaustria.org>
2019-05-29 19:58:40 +0200

    Falten Exceptions

--- projecte/src/Alumne.java
+++ projecte/src/Alumne.java
@@ -1,4 +1,53 @@
 
 public class Alumne {
+	private String nom;
+	private int eval1;
+	private int eval2;
+	private int eval3;
 
+	public Alumne(String nom, int eval1, int eval2, int eval3) {
+		super();
+		this.nom = nom;
+		this.eval1 = eval1;
+		this.eval2 = eval2;
+		this.eval3 = eval3;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public int getEval1() {
+		return eval1;
+	}
+	
+	public void setEval1(int eval1) {
+		this.eval1 = eval1;
+	}
+	
+	public int getEval2() {
+		return eval2;
+	}
+	
+	public void setEval2(int eval2) {
+		this.eval2 = eval2;
+	}
+	
+	public int getEval3() {
+		return eval3;
+	}
+	
+	public void setEval3(int eval3) {
+		this.eval3 = eval3;
+	}
+	
+	@Override
+	public String toString() {
+		return nom + "\t\t\t" + eval1 + "\t\t" + eval2 + "\t\t" + eval3 + "\t\t" + (eval1+eval2+eval3)/3;
+	}
+	
 }
--- projecte/src/ProjecteMain.java
+++ projecte/src/ProjecteMain.java
@@ -1,237 +1,182 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ProjecteMain {
-
-	String seleccio;
+	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	static ArrayList<Alumne> institut = new ArrayList<>();
 	
-	public static String menu() {
-		System.out.println("PROJECTE MANTENIMENT NOTES ASSIGNATURA\n\nMenú:\n\na-Afegir.\nm-Mostrar.\nt-Aprovar.\nn-Canviar nom.\nc-Canviar nota.\nQ-Ajuda.\nx-Sortir.\n");
-		System.out.println("Quina opció vols escollir? ");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String A = reader.readLine();
-		seleccio = Integer.parseInt(A);
-      
-    global dades
-    global alumnes
-    global total
-    
-    	if (seleccio == "a") {
-    		alumnes = pregunta();
-    		inserir(alumnes,dades,eval_1,eval_2,eval_3);
-    	}
+	public static void main(String[] args) throws IOException{
+				
+		int seleccio = 9;
+		
+		while(seleccio != 0){
+		System.out.println("PROJECTE MANTENIMENT NOTES ASSIGNATURA");
+		System.out.println("=======================================\n");
+		System.out.println("Quina opció vols escollir?\n");
+		System.out.println("1) Afegir alumne");
+		System.out.println("2) Mostrar informe de notes");
+		System.out.println("3) Aprovar a tots");
+		System.out.println("4) Canviar el nom");
+		System.out.println("5) Canviar la nota");
+		System.out.println("6) Ajuda");
+		System.out.println("0) Sortir");
+		String num = reader.readLine();
+		seleccio = Integer.parseInt(num);
 
-    	if (seleccio == "m") {
-    		if (total != 0) {
-    			mostrar(total,dades,eval_1,eval_2,eval_3);
-    		} else {
-    			System.out.println("Error! No hi han dades a mostrar.\n");
-    		}
-    	}
-    
-    	if (seleccio == "t") {
-    		if (total != 0) {
-    			aprovar(eval_1,eval_2,eval_3);
-    			mostrar(total,dades,eval_1,eval_2,eval_3);
-    		} else {
-    			System.out.println("Error! No hi han notes per aprovar.\n");
-    		}
-    	}
-    
-    	if (seleccio == "n") {
-    		try {
-    			index = buscar(dades);
-    			renombrar(dades,index);
-    		} catch (NameErrorException nominc) {
-				System.out.println("Error! Aquest alumne no està afegit.\n");
-    		}
-    	}
-    		
-    	if (seleccio == "c") {
-    		try {
-    			index = buscar(dades);
-    			notes = evaluacio();
-    			canvi(index,notes);
-    		} catch (NameErrorException nominc) {
-    			System.out.println("Error! Aquest alumne no està afegit.\n");
-    		}
-    	}
-        
-    	if (seleccio == "Q") {
-    		ajuda(seleccio);
-    	}
-        
-    	return seleccio;
-    		
-	}
-    
-	public static Alumne pregunta() {
-		while (True) {
-			try {
-				//Este paso con dar el número de valores totales del ArrayList es suficioente.
-				System.out.println("Quants alumnes tens? ");
-				String n1 = reader.readLine();
-				A = Integer.parseInt(n1);
-				while (Alumne <1 || Alumne >50) {
-					System.out.println("Error! El número d'alumnes ha de ser entre 1 i 50.");
-					System.out.println("Quants alumnes tens? ");
-				}
-				return Alumne;
-			} catch (NameErrorException numinc) {
-					System.out.println("Error! Has d'afegir un número d'alumnes.");
-			} catch (Exception n) {
-					System.out.println("Error! Has d'afegir un número d'alumnes.");
+		switch(seleccio){
+		case 1:
+			 if (afegirAlumne()) {
+                    System.out.println("Alumne i notes afegides correctament.\n");
+                } else {
+                    System.out.println("Hi ha hagut un problema amb la inserció de l'alumne, torna-ho a provar.\n");
+                }
+			break;
+		case 2:
+			if (institut.isEmpty()) {
+				System.out.println("No hi ha cap alumne enregistrat!\n");
+			} else {
+				System.out.println("alumnes\t\t\t1ra\t\t2na\t\t3ra\t\tfinal");
+				System.out.println("------------------------------------------------------------------------------");
+				Iterator<Alumne> iter = institut.iterator();
+                while (iter.hasNext()) {
+                    Alumne p = iter.next();
+                    System.out.println(p.toString());
+                }
+                System.out.println(" ");
 			}
+			break;
+		case 3:
+			if (institut.isEmpty()) {
+                System.out.println("No hi ha cap alumne enregistrat!\n");
+            } else {
+            	Iterator<Alumne> iter = institut.iterator();
+                while (iter.hasNext()) {
+                    Alumne p = iter.next();
+                    if (p.getEval1() < 5) {
+                    	p.setEval1(5);
+                    }
+                    if (p.getEval2() < 5) {
+                    	p.setEval2(5);
+                    }
+                    if (p.getEval3() < 5) {
+                    	p.setEval3(5);
+                    }
+                }
+            }
+			break;						
+		case 4:
+			if (institut.isEmpty()) {
+                System.out.println("No hi ha cap alumne enregistrat!\n");
+            } else {
+            	Iterator<Alumne> iter = institut.iterator();
+            	System.out.println("De quin alumne vols canviar el nom? ");
+            	String nomAlumne = reader.readLine();
+            	boolean nomCanviat = false;
+            	
+            	while (iter.hasNext() && nomCanviat == false) {
+	                Alumne a = iter.next();
+
+	            	if (nomAlumne.equals(a.getNom())) {
+	                	System.out.println("Quin nou nom vols posar? ");
+	                	String nomNou = reader.readLine();
+	            		a.setNom(nomNou);
+	            		System.out.println("Nom canviat correctament.\n");
+	            		nomCanviat = true;
+            		}
+	            }
+            	 if (nomCanviat == false) {
+         			System.out.println("Error! Aquest alumne no està enregistrat.\n");
+         		}
+            }
+			break;
+		case 5:
+			if (institut.isEmpty()) {
+                System.out.println("No hi ha cap alumne enregistrat!\n");
+            } else {
+            	System.out.println("De quin alumne vols canviar les notes? ");
+            	String nomAlumne = reader.readLine();
+            	boolean notes = false;
+            	
+            	Iterator<Alumne> iter = institut.iterator();
+                while (iter.hasNext() && notes == false) {
+                Alumne a = iter.next();
+       
+	            	if (nomAlumne.equals(a.getNom())) {
+	            		System.out.println("De quina evaluació vols fer el canvi (1, 2, 3)? ");
+	            		String eval = reader.readLine();
+	            		int evaluacio = Integer.parseInt(eval);
+	            		if (evaluacio == 1) {
+	            			System.out.println("Quina nota nova vols possar? ");
+	            			String n1 = reader.readLine();
+	            			int nova1 = Integer.parseInt(n1);
+	            			a.setEval1(nova1);
+	            			System.out.println("Nota canviada correctament.\n");
+	            			notes = true;
+	            		}
+	            		if (evaluacio == 2) {
+	            			System.out.println("Quina nota nova vols possar? ");
+	            			String n2 = reader.readLine();
+	            			int nova2 = Integer.parseInt(n2);
+	            			a.setEval2(nova2);
+	            			System.out.println("Nota canviada correctament.\n");
+	            			notes = true;
+	            		}
+	            		if (evaluacio == 3) {
+	            			System.out.println("Quina nota nova vols possar? ");
+	            			String n3 = reader.readLine();
+	            			int nova3 = Integer.parseInt(n3);
+	            			a.setEval3(nova3);
+	            			System.out.println("Nota canviada correctament.\n");
+	            			notes = true;
+	                    }
+	            	
+	            	}
+                }
+            	if (notes == false) {
+                    System.out.println("Error! Aquest alumne no està enregistrat.\n");
+                }
+            }
+			break;	
+		case 6:
+			System.out.println("\nAJUDA DEL MENÚ:\n\n(1-Afegir alumne) Afegeix el nom de l'alumne i les notes de cada evaluació.\n(2-Mostrar informe de notes) Mostra per pantalla les dades de tots els alumnes amb les notes de cada evaluació i la seva mitjana final.\n(3-Aprovar a tots) Puja a 5 les notes de totes les evaluacions suspeses.\n(4-Canviar el nom) Canvia el nom d'algun alumne existent.\n(5-Canviar la nota) Canvia la nota d'alguna avaluació d'un alumne concret.\n(0-Sortir) Surt del programa." + "\n");
+			break;
+		case 0:
+			System.out.println("Adeu!");
+			break;	
+		default:
+			System.out.println("OPCIO INCORRECTA!!!");
+		}
 		}
 	}
-    
-	public static void inserir(Alumne,dades) {
-		//Método asociado a la clase Alumne, recogerá los datos introducidos.
-		for (i=0; i < Alumne.size(); i++) {
-			correctName = False;
-			while (correctName == False) {
-				try {
-					System.out.println("Introdueix el nom de l'alumne: ");
-					String n1 = reader.readLine();
-					A = Integer.parseInt(n1);
-					dades.add(nom);
-					correctName = True;
-				} catch (SyntaxErrorException maladada) {
-					System.out.println("Error! Has d'afegir un nom.");
-				} catch (Exception n) {
-					System.out.println("Error! Has d'afegir un nom.");
-				}
-			}
-		correct1 = False;
-		correct2 = False;
-		correct3 = False;
-		while correct1 == False:
-			try:
-				n1 = input("Introdueix la nota de la primera evaluació: ")
-				while n1 < 0 or n1 > 10:
-					print ("Error! La nota ha de ser entre 0 i 10.")
-					n1 = input("Introdueix la nota de la primera evaluació: ")
-				eval_1.append(n1)
-				correct1 = True
-		
-			except NameError:
-				print ("Error! Has d'introduir un número.")
-			except:
-				print ("Error! Has d'introduir un número.")
-		
-		while correct2 == False:
-			try:  
-				n2 = input("Introdueix la nota de la segona evaluació: ")
-				while n2 < 0 or n2 > 10:
-					print ("Error! La nota ha de ser entre 0 i 10.")
-					n2 = input("Introdueix la nota de la segona evaluació: ")
-				eval_2.append(n2)
-				correct2 = True
+
+	public static boolean afegirAlumne() {
+		try{
+			System.out.println("Quin és el nom de l'alumne?");
+			String nom = reader.readLine();
+
+			System.out.println("Quina és la nota de la primera evaluació?");
+			String nota1 = reader.readLine();
+			int eval1 = Integer.parseInt(nota1);
+
+
+			System.out.println("Quina és la nota de la segona evaluació?");
+			String nota2 = reader.readLine();
+			int eval2 = Integer.parseInt(nota2);
 			
-			except NameError:
-				print ("Error! Has d'introduir un número.")
-			except:
-				print ("Error! Has d'introduir un número.")
+			System.out.println("Quina és la nota de la tercera evaluació?");
+			String nota3 = reader.readLine();
+			int eval3 = Integer.parseInt(nota3);
+			 
+			Alumne alumne = new Alumne(nom, eval1, eval2, eval3);
 			
-		while correct3 == False:
-			try:
-				n3 = input("Introdueix la nota de la tercera evaluació: ")
-				while n3 < 0 or n3 > 10:
-					print ("Error! La nota ha de ser entre 0 i 10.")
-					n3 = input("Introdueix la nota de la tercera evaluació: ")
-				eval_3.append(n3)
-				correct3 = True
-			
-			except NameError:
-				print ("Error! Has d'introduir un número.")
-			except:
-				print ("Error! Has d'introduir un número.")
-			
-    print("Ja estan totes les dades desades." + "\n")
-    global total
-    total += alumnes
+			institut.add(alumne);
+
+			return true;
+		}
+		catch(Exception e){
+			return false;
 		}
 	}
-	
-    public static void mostrar(total,dades,eval_1,eval_2,eval_3) {
-    	del resultats[:]
-    	for i in range (total):
-    		final = (eval_1[i] + eval_2[i] + eval_3[i]) / 3
-    		resultats.append(str(dades[i]) + "\t\t\t" + str(eval_1[i]) + "\t\t" + str(eval_2[i]) + "\t\t" + str(eval_3[i]) + "\t\t" + str(float(final)))
-                
-    		System.out.println("alumnes			1ra		2na		3ra		final");
-    		System.out.println("------------------------------------------------------------------------------");
-    	for i in resultats:
-    		System.out.println((i) + "\n");
-    
-    public static void aprovar(eval_1,eval_2,eval_3) {
-    	for i in range(len(eval_1)):
-    		if eval_1[i] < 5:
-    			eval_1[i] = 5
-    	for i in range(len(eval_2)):
-    		if eval_2[i] < 5:
-    			eval_2[i] = 5
-    	for i in range(len(eval_3)):
-    		if eval_3[i] < 5:
-    			eval_3[i] = 5
-    }
-    
-    public static int buscar(dades) {
-    	posicio = raw_input("De quin alumne vols fer canvis? ")
-    	for i in range(len(dades)):
-    		if posicio == dades[i]:
-    			index = i
-    			return index
-    	while posicio != dades[i]:
-    		System.out.println("Error! Aquest alumne no està registrat.");
-    		buscar(dades)
-    }
 			
-    public static int renombrar(dades,index) {
-			nom2 = raw_input("Quin nou nom li vols possar? ")
-			dades[index] = nom2
-			return dades;
-    }
- 
-    public static int evaluacio() {
-    	notes = raw_input("La nota de quina evaluació vols canviar? (1, 2, 3): ")
-    	while notes != "1" and notes != "2" and notes != "3":
-    		System.out.println("Error! Aquesta evaluació no existeix.");
-    		notes = raw_input("La nota de quina evaluació vols canviar? ")
-    	return notes;
-    }
-
-    public static void canvi(index,notes) {    
-    	nova = input("Introdueix la nova nota: ")
-    	while nova < 0 or nova > 10:
-    		System.out.println("Error! Les notes han de ser entre 0 i 10.");
-        	nova = input("Introdueix la nova nota: ")
-        if notes == "1":
-        	eval_1[index] = nova
-        if notes == "2":
-        	eval_2[index] = nova
-        if notes == "3":
-        	eval_3[index] = nova
-    }
-    
-    public static void ajuda(Q) {
-    	System.out.println("\nAJUDA DEL MENÚ:\n\n(a-Afegir) Afegeix el nom de l'alumne i les notes de cada evaluació.\n(m-Mostrar) Mostra per pantalla les dades de tots els alumnes amb les notes de cada evaluació i la seva mitjana final.\n(t-Aprovar) Puja a 5 les notes de totes les evaluacions suspeses.\n(n-Canviar nom) Canvia el nom d'algun alumne existent.\n(c-Canviar nota) Canvia la nota d'alguna avaluació d'un alumne concret.\n(x-Sortir) Surt del programa." + "\n");
-    }
-    
-dades=[]
-eval_1=[]
-eval_2=[]
-eval_3=[]
-resultats=[]
-alumnes = 0
-total = 0
-
-	seleccio = menu()
-	if (seleccio != "a" || seleccio != "m" || seleccio != "t" || seleccio != "c" || seleccio != "n" || seleccio != "Q") {
-		System.out.println("Error! Aquest valor no selecciona cap acció del menú.\n");
-	}
-	while (seleccio != "x") {
-		seleccio = menu();
-	System.out.println("\nFi del programa");
-	}
 }
